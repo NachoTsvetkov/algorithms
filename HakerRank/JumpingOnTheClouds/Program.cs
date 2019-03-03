17b70950360fb56da582e048cb7478e8ad14699e
Nacho Tsvetkov <nacho.tsvetkov@gmail.com>
2019-03-03 13:04:32 +0200

    Jumping On Clouds Implemented

---
+++ HakerRank/JumpingOnTheClouds/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System;
+using System.Linq;
+
+class Solution
+{
+
+    // Complete the sockMerchant function below.
+    static int JumpingOnClouds(int[] c)
+    {
+        var jumps = 0;
+        for (int i = 0; i < c.Length; )
+        {
+            var leapIsSafe = c.Length > i + 2 && c[i + 2] != 1;
+            var stepIsSafe = c.Length > i + 1 && c[i + 1] != 1;
+
+            if (!(leapIsSafe || stepIsSafe))
+            {
+                break;
+            }
+
+            var move = leapIsSafe ? 2 : 1;
+            jumps++;
+            i += move;
+        }
+
+        return jumps;
+    }
+
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] c = Array.ConvertAll(Console.ReadLine().Split(' '), cTemp => Convert.ToInt32(cTemp))
+        ;
+        int result = JumpingOnClouds(c);
+
+        Console.WriteLine(result);
+    }
+}

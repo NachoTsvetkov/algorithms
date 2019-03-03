19dee6f1ac4715a9f101288bac9bef05a8d9494c
Nacho Tsvetkov <nacho.tsvetkov@gmail.com>
2019-03-03 13:24:55 +0200

    Repeated String implemented

---
+++ HakerRank/RepeatedString/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System;
+using System.Linq;
+
+class Solution
+{
+
+    // Complete the sockMerchant function below.
+    // Complete the repeatedString function below.
+    static long repeatedString(string s, long n)
+    {
+        long occurences = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == 'a')
+            {
+                occurences++;
+            }
+        }
+
+        occurences *= n / s.Length;
+        var remainder = n % s.Length;
+
+        for (int i = 0; i < remainder; i++)
+        {
+            if (s[i] == 'a')
+            {
+                occurences++;
+            }
+        }
+
+        return occurences;
+    }
+
+    static void Main(string[] args)
+    {
+        string s = Console.ReadLine();
+
+        long n = Convert.ToInt64(Console.ReadLine());
+
+        long result = repeatedString(s, n);
+
+        Console.WriteLine(result);
+    }
+}

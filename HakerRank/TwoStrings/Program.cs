c795fffe7a564dff7d72ef160b955c77c8622fc9
Nacho Tsvetkov <nacho.tsvetkov@gmail.com>
2019-03-06 17:06:35 +0200

    Two strings implemented

---
+++ HakerRank/TwoStrings/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the twoStrings function below.
+    static string twoStrings(string s1, string s2)
+    {
+        var longer = s1;
+        var shorter = s2;
+
+        if (s2.Length > s1.Length)
+        {
+            longer = s2;
+            shorter = s1;
+        }
+
+
+        var longerSet = new HashSet<char>(longer.ToCharArray());
+
+        var hasSubstring = false;
+
+        foreach (var character in shorter)
+        {
+            if (longerSet.Contains(character))
+            {
+                hasSubstring = true;
+                break;
+            }
+        }
+
+        return hasSubstring ? "YES" : "NO";
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string s1 = Console.ReadLine();
+
+            string s2 = Console.ReadLine();
+
+            string result = twoStrings(s1, s2);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}

e676bb6b2363da835ca0fa6e68026fb1d86595f1
nacho.tsvetkov <nacho.tsvetkov@gmail.com>
2019-03-14 13:57:20 +0200

    Alternating chars implemented

---
+++ HakerRank/AlternatingChars/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Solution
+{
+
+    // Complete the alternatingCharacters function below.
+    static int AlternatingCharacters(string s)
+    {
+        var skipCount = 0;
+        for (int i = 0; i < s.Length - 1; i++)
+        {
+            var current = s[i];
+            var next = s[i + 1];
+
+            if (current == next)
+            {
+                skipCount++;
+            }
+        }
+
+        return skipCount;
+    }
+
+    static void Main(string[] args)
+    {
+        int q = Convert.ToInt32(Console.ReadLine());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string s = Console.ReadLine();
+
+            int result = AlternatingCharacters(s);
+
+            Console.WriteLine(result);
+        }
+    }
+}

b2466386505aa53c7dd26f83ac7e99abd83363d7
nacho.tsvetkov <nacho.tsvetkov@gmail.com>
2019-03-27 20:05:27 +0200

    Linked Lists implementations

---
+++ HakerRank/BalancedBrackets/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the isBalanced function below.
+    static string isBalanced(string s)
+    {
+        var matches = new Dictionary<char, char>
+        {
+            { '{', '}' },
+            { '[', ']' },
+            { '(', ')' }
+        };
+
+        var stack = new Stack<char>();
+        foreach (var c in s)
+        {
+            if (matches.Keys.Contains(c))
+            {
+                stack.Push(c);
+            }
+            else
+            {
+                if (stack.Count == 0)
+                {
+                    return "NO";
+                }
+
+                var openingBracket = stack.Pop();
+                if (c != matches[openingBracket])
+                {
+                    return "NO";
+                }
+            }
+        }
+
+        if (stack.Count > 0)
+        {
+            return "NO";
+        }
+
+        return "YES";
+    }
+
+    static void Main(string[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string s = Console.ReadLine();
+
+            string result = isBalanced(s);
+
+            Console.WriteLine(result);
+        }
+
+    }
+}
---
+++ HakerRank/InsertIntoDoubleLinkedList/Program.cs
@@ -0,0 +1,151 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    class DoublyLinkedListNode
+    {
+        public int data;
+        public DoublyLinkedListNode next;
+        public DoublyLinkedListNode prev;
+
+        public DoublyLinkedListNode(int nodeData)
+        {
+            this.data = nodeData;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    class DoublyLinkedList
+    {
+        public DoublyLinkedListNode head;
+        public DoublyLinkedListNode tail;
+
+        public DoublyLinkedList()
+        {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void InsertNode(int nodeData)
+        {
+            DoublyLinkedListNode node = new DoublyLinkedListNode(nodeData);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                this.tail.next = node;
+                node.prev = this.tail;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    static void PrintDoublyLinkedList(DoublyLinkedListNode node, string sep)
+    {
+        while (node != null)
+        {
+            Console.Write(node.data);
+
+            node = node.next;
+
+            if (node != null)
+            {
+                Console.Write(sep);
+            }
+        }
+    }
+
+    // Complete the sortedInsert function below.
+
+    /*
+     * For your reference:
+     *
+     * DoublyLinkedListNode {
+     *     int data;
+     *     DoublyLinkedListNode next;
+     *     DoublyLinkedListNode prev;
+     * }
+     *
+     */
+    static DoublyLinkedListNode sortedInsert(DoublyLinkedListNode head, int data)
+    {
+        var current = head;
+        var newNode = new DoublyLinkedListNode(data);
+
+        if (head == null)
+        {
+            return newNode;
+        }
+
+        if (head.data > data)
+        {
+            newNode.next = head;
+            head.prev = newNode;
+
+            return newNode;
+        }
+
+        while (current.next != null && current.next.data < data)
+        {
+            current = current.next;
+        }
+
+        if (current.next == null)
+        {
+            current.next = newNode;
+            newNode.prev = current;
+        }
+        else
+        {
+            newNode.next = current.next;
+            newNode.prev = current;
+            current.next.prev = newNode;
+            current.next = newNode;
+        }
+
+        return head;
+    }
+
+    static void Main(string[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            DoublyLinkedList llist = new DoublyLinkedList();
+
+            int llistCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < llistCount; i++)
+            {
+                int llistItem = Convert.ToInt32(Console.ReadLine());
+                llist.InsertNode(llistItem);
+            }
+
+            int data = Convert.ToInt32(Console.ReadLine());
+
+            DoublyLinkedListNode llist1 = sortedInsert(llist.head, data);
+
+            PrintDoublyLinkedList(llist1, " ");
+            Console.WriteLine();
+        }
+    }
+}

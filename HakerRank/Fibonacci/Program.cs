b6014ff2e8dfb4317d8e343bc5461b029bfda930
nacho.tsvetkov <nacho.tsvetkov@gmail.com>
2019-04-10 16:34:48 +0300

    Fibonacci implementation

---
+++ HakerRank/Fibonacci/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution
+{
+
+    public static int Fibonacci(int n)
+    {
+        if (n == 0)
+        {
+            return 0;
+        }
+
+        var sequence = new int[n + 1];
+        sequence[0] = 0;
+        sequence[1] = 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            sequence[i] = sequence[i - 1] + sequence[i - 2];
+        }
+
+        var value = sequence[n];
+        return value;
+    }
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(Fibonacci(n));
+    }
+}
+
